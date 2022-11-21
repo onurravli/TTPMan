ee48dd7daf717bb2e9c1b55010454eda958e77cc
onurravli <onurravli@hotmail.com>
2022-11-21 10:12:42 +0300

    Added help function and error handling.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "string.h"
 #include "TTPMan.h"
 
 using std::cout;
@@ -8,14 +9,22 @@ using std::endl;
 
 void usage() {
     cerr << "Usage: TTPMan [-s | --status] [-m <mode> | --mode <mode>]" << endl;
+    exit(1);
+}
+
+void help() {
+    cout << "TTPMan - Throttle Thermal Policy Manager" << endl;
+    cout << "Usage: TTPMan [-s | --status] [-m <mode> | --mode <mode>]" << endl;
 }
 
 int main(int argc, char** argv) {
     auto* ttpMan = new TTPMan();
-    if(argc == 1) {
+    if (argc == 1) {
         usage();
+    } else if (argc == 2 && (strcmp(argv[1], "-h") == 0) | strcmp(argv[1], "--help") == 0) {
+        help();
     } else {
-        return 0;
+        usage();
     }
     return 0;
 }
